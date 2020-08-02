f610f086040e6eb1e2951a657df60a00b59d981c
Ashkrit <ashkrit@gmail.com>
2020-08-02 22:10:47 +0800

    Refactor - Add example

--- src/main/java/org/domainspecific/dsl/statemachine/StateMachine.java
+++ src/main/java/org/domainspecific/dsl/statemachine/StateMachine.java
@@ -6,6 +6,30 @@ import org.domainspecific.dsl.statemachine.rule.MatchRules;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <pre>
+ *  StateMachine<String, String, String> cbMachine = machine("Circuit Breaker", machine -> {
+ *             machine.startWith("OPEN");
+ *
+ *             machine.at("OPEN", rule -> {
+ *                 rule.when("connect", failOp, "OPEN", "CLOSED");
+ *             });
+ *
+ *             machine.at("CLOSED", rule -> {
+ *                 rule.when("connect", s -> { }, "CLOSED");
+ *                 rule.when("re-connect", s -> { }, "HALF-OPEN");
+ *             });
+ *
+ *             machine.at("HALF-OPEN", rule -> {
+ *                 rule.when("connect", s -> { }, "CLOSED");
+ *                 rule.when("re-open", s -> { }, "OPEN");
+ *
+ *             });
+ *
+ *         });
+ *
+ * </pre>
+ */
 public class StateMachine<State, Event, Message> {
     private final String machineName;
     private final List<MatchRule> rules = new ArrayList<>();
